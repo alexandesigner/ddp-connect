6b3b9f54e63850a9775f4ff07f05460acc0d42e6
Alexandre <alissonplus@gmail.com>
2017-03-30 17:02:42 -0300

    inicio teste BI

--- connect/client/main.js
+++ connect/client/main.js
@@ -2,26 +2,46 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
 import { remote } from '../imports/startup/remote';
-
 import { PainelHtml } from '../imports/api/collections.js';
 
-Template.list.onCreated(function listOnCreated(){
+// LIST 1
+Template.list1.onCreated(function list1OnCreated(){
   remote.subscribe('painelHtml');
-})
+  console.log(PainelHtml.find({}));
+});
 
-Template.list.helpers({
-  list: function () {
+Template.list1.helpers({
+  list1: function () {
     // Perform a reactive database query against minimongo
     return PainelHtml.find({})
   }
 });
 
-Template.button.events({
-    'click #save': (event, instance) => {
+Template.list1.events({
+    'click #save1': (event, instance) => {
         event.preventDefault();
-        console.log('click');
-        remote.call('setTestDdp');
+        console.log('click 1');
+        remote.call('setTestDdp1');
+    }
+});
+
+// LIST 2
+Template.list2.onCreated(function list2OnCreated(){
+  remote.subscribe('painelHtml');
+});
+
+Template.list2.helpers({
+  list2: function () {
+    // Perform a reactive database query against minimongo
+    return PainelHtml.find({teste:2})
+  }
+});
+
+Template.list2.events({
+    'click #save2': (event, instance) => {
+        event.preventDefault();
+        console.log('click 2');
+        remote.call('setTestDdp2');
     }
 });
--- connect/imports/api/collections.js
+++ connect/imports/api/collections.js
@@ -1,5 +1,4 @@
 import { Mongo } from 'meteor/mongo';
-
 import { remote } from '../startup/remote.js';
 
 export const PainelHtml = new Mongo.Collection('painelHtml', remote);
